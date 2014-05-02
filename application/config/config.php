102e3295bacef85f3fe5500c3bb805522d09fc27
Robbie Bardijn <robbie.bardijn@gmail.be>
2014-05-02 09:12:55 +0200

    url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://local.oldportfolio/';
+$config['base_url']	= 'http://games.robbiebardijn.be/';
 
 /*
 |--------------------------------------------------------------------------
