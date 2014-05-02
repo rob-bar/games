aa351877604b9cc3edf727671d87d986e63ca357
Robbie Bardijn <robbie.bardijn@gmail.be>
2014-05-02 08:42:07 +0200

    tracking

--- application/views/tetris.php
+++ application/views/tetris.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html>
-	<?php $this->load->view("tetrishead.php"); ?>
-	<body>
-		<?php $this->load->view("tetrisbody.php"); ?>
-		<?php //$this->load->view("foot.php"); ?>
-	</body>
+  <?php $this->load->view("tetrishead.php"); ?>
+
+  <body>
+    <?php $this->load->view("tetrisbody.php"); ?>
+    <?php $this->load->view("foot.php"); ?>
+  </body>
 </html>
