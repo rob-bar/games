5dac8626bb58fe1f0b5afe04b227cd65d19d282c
Robbie Bardijn <robbie.bardijn@gmail.be>
2014-05-02 08:49:25 +0200

    tracking

--- application/views/foot.php
+++ application/views/foot.php
@@ -4,12 +4,15 @@ echo $footextra;
 
 <?php if (ENVIRONMENT == $GLOBALS["env_prod"]): ?>
   <script type="text/javascript">
-    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-      function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-      e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-      e.src='//www.google-analytics.com/analytics.js';
-      r.parentNode.insertBefore(e,r)}(window, document, 'script', 'ga'));
-      ga('create','UA-27483596-1', {'cookieDomain': 'robbiebardijn.be'});
-      ga('send','pageview');
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-27483596-1']);
+    _gaq.push(['_setDomainName', 'robbiebardijn.be']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
   </script>
 <?php endif; ?>
